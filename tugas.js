ee1c8f2656bbc7d7f180bfe3c027609e6906171a
fizi235 <fizinur2@gmail.com>
2025-01-15 10:59:15 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,19 @@
+let nama = "Fizi Nur Aminati";
+let alamat = "Tambak roto Pentalan rt.03/rw,03";
+let usia = "15";
+let ttl = "Demak 04-10-2009";
+let gender = "Perempuan";
+let agama = "Islam";
+let email = "fizinur2@gmail.com";
+let berat = "55";
+let tinggi = "152"
+let hobi = "menyanyi"
+let cita = "guru"
+let pekerjaan = "pelajar"
+let sekolah = "smk n 1 sayung"
+let jurusan = "pplg"
+
+document.write(" "+" nama saya adalah " + nama +" "+" saya tinggal di "+ alamat +" " +"pada tahun 2024 ini usia saya adalah "+ usia + "tahun "
++" saya lahir di " + ttl + " jenis kelamin saya adalah " + gender + " saya beragama " + agama + " alamat email saya adalah " + email + "saya memiliki berat badan " + berat + "kg dengan tinggi " + tinggi
++ "cm " +" saya sangat senang " + hobi +" karena saya memiliki cita-cita " + cita +" saat ini adalah saya adalah seorang " + pekerjaan +" yang bersekolah di " + sekolah
++" pada jurusan " + jurusan)
